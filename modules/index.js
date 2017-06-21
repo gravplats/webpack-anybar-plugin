4d5a4f56248e98449d342a3b84324e80448cea1e
Mattias Rydengren <mattias.rydengren@coderesque.com>
2017-06-21 15:00:10 +0200

    Move AnyBar check/autostart to 'environment' compiler event hook.

--- modules/index.js
+++ modules/index.js
@@ -34,7 +34,40 @@ function WebpackAnybarPlugin(options) {
 }
 
 WebpackAnybarPlugin.prototype.apply = function(compiler) {
-    this.checkForAnyBarApp();
+    compiler.plugin('environment', () => {
+        const { applicationPath, autoStart, port } = this.options;
+        /* eslint-disable no-console */
+        if (autoStart) {
+            try {
+                // is AnyBar running and listening to the specified port?
+                cp.exec(`lsof -ti udp:${port}`, function lsofCallback(err) {
+                    // 'lsof' returns an error if it cannot find what it's looking for.
+                    if (err !== null) {
+                        if (fs.existsSync(applicationPath)) {
+                            return cp.exec(`ANYBAR_PORT=${port} open -na ${applicationPath}`, function openAnyBarCallback(err) {
+                                if (err !== null) {
+                                    return;
+                                }
+
+                                console.log(`webpack-anybar-plugin: Starting AnyBar on port ${port}`);
+                            });
+                        } else {
+                            console.log(
+                                chalk.cyan(
+                                    `webpack-anybar-plugin: Unable to find AnyBar application at "${applicationPath}".` +
+                                    ' Download it from "https://github.com/sfsam/AnyBar" for a better webpack experience. ' +
+                                    'See "https://github.com/mrydengren/webpack-anybar-plugin" for configuration options.'
+                                )
+                            );
+                        }
+                    } else {
+                        console.log(`webpack-anybar-plugin: AnyBar is listening on port ${port}`);
+                    }
+                });
+            } catch (e) {} // eslint-disable-line no-empty
+        }
+        /* eslint-enable no-console */
+    });
 
     compiler.plugin('watch-run', (compiler, callback) => {
         const { status } = this.options;
@@ -51,41 +84,6 @@ WebpackAnybarPlugin.prototype.apply = function(compiler) {
 WebpackAnybarPlugin.prototype.broadcast = function(status, info) {
     const { port } = this.options;
     return anybar(typeof status === 'function' ? status(info) : status, { port });
-};
-
-WebpackAnybarPlugin.prototype.checkForAnyBarApp = function() {
-    const { applicationPath, autoStart, port } = this.options;
-    /* eslint-disable no-console */
-    if (autoStart) {
-        try {
-            // is AnyBar running and listening to the specified port?
-            cp.exec(`lsof -ti udp:${port}`, function lsofCallback(err) {
-                // 'lsof' returns an error if it cannot find what it's looking for.
-                if (err !== null) {
-                    if (fs.existsSync(applicationPath)) {
-                        return cp.exec(`ANYBAR_PORT=${port} open -na ${applicationPath}`, function openAnyBarCallback(err) {
-                            if (err !== null) {
-                                return;
-                            }
-
-                            console.log(`webpack-anybar-plugin: Starting AnyBar on port ${port}`);
-                        });
-                    } else {
-                        console.log(
-                            chalk.cyan(
-                                `webpack-anybar-plugin: Unable to find AnyBar application at "${applicationPath}".` +
-                                ' Download it from "https://github.com/sfsam/AnyBar" for a better webpack experience. ' +
-                                'See "https://github.com/mrydengren/webpack-anybar-plugin" for configuration options.'
-                            )
-                        );
-                    }
-                } else {
-                    console.log(`webpack-anybar-plugin: AnyBar is listening on port ${port}`);
-                }
-            });
-        } catch (e) {} // eslint-disable-line no-empty
-    }
-    /* eslint-enable no-console */
 };
 
 module.exports = WebpackAnybarPlugin;
